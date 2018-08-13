87cb5aacf6b090cefd61b207511bc0576ac66493
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2018-08-13 21:31:27 +0200

    review fixes

---
+++ source/b/Additions/NettyServerProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xamarin.Grpc;
+
+namespace Xamarin.Grpc.Netty
+{
+
+    // Metadata.xml XPath class reference: path="/api/package[@name='io.grpc.netty']/class[@name='NettyServerProvider']"
+    //public sealed partial class NettyServerProvider 
+    //{
+        //protected virtual Xamarin.Grpc.ServerBuilder BuilderForPort(int p0)
+        //{
+        //    throw new NotImplementedException("BuilderForPort");
+        //}
+    //}
+}
